701511230c675fcdf01a3f059bbbd4a5e1848285
Marcos Lorente <b5SSmN8z>
2021-06-18 14:54:47 +0200

    Adds vars and constant .Class 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,36 @@
 <script>
-	export let name;
-	let nombre = 'Makros';
+    let nombre = 'Makros'
+    const nombre_2 = 'Sandre'
+    nombre = "<strong style='color: firebrick'>Pedro</strong>"
+	const url = "https://api.com"
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<h2>Test título H2 {nombre}</h2>
+    <p>{@html nombre}</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	h2{
-		color: red;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h2 {
+        color: red;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'conchita'
-	}
+	target: document.body
 });
 
 export default app;
